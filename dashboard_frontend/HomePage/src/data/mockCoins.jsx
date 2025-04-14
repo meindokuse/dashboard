06284c6d0acced75347aebd738445687074f1e81
George <george2040@mail.ru>
2025-04-14 21:30:16 +0300

    Реализовал стартовую домашнюю страницу

---
+++ dashboard_frontend/HomePage/src/data/mockCoins.jsx
@@ -0,0 +1,18 @@
+export const mockCoins = [
+    {
+      id: 'bitcoin',
+      name: 'Bitcoin',
+      symbol: 'btc',
+      current_price: 50000,
+      price_change_percentage_24h: 2.5,
+      image: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png'
+    },
+    {
+      id: 'ethereum',
+      name: 'Ethereum',
+      symbol: 'eth',
+      current_price: 3000,
+      price_change_percentage_24h: -1.2,
+      image: 'https://assets.coingecko.com/coins/images/279/large/ethereum.png'
+    },
+  ]
